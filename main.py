35f1825d054a9970e1e89e21b388f9c8e1f10baa
Mizzia Pontino <tengyteng22@gmail.com>
2022-10-18 13:24:46 +0800

    Revision date: 10/18 1:24pm

--- main.py
+++ main.py
@@ -20,11 +20,10 @@ from itertools import cycle, islice
 from pandas.plotting import parallel_coordinates
 from sklearn.preprocessing import LabelEncoder
 
-class df_class:
-    df1=pd.read_excel('ABBREV.xlsx')
-    example_df = df1[['NDB_No','Water_(g)','Energ_Kcal',
-    'Protein_(g)','Lipid_Tot_(g)','Ash_(g)','Carbohydrt_(g)']]
-    max = int(example_df.iloc[-1]['NDB_No'])
+dataframe = pickle.load(open('example.pkl', 'rb'))
+example_df = dataframe[['NDB_No','Water_(g)','Energ_Kcal',
+'Protein_(g)','Lipid_Tot_(g)','Ash_(g)','Carbohydrt_(g)']]
+max = int(dataframe.iloc[-1]['NDB_No'])
 
 app = Flask(__name__)
 
@@ -34,8 +33,8 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    """Grabs the input values and uses them to make prediction"""
-    foodId = df_class.max+1
+    '''User's input from the form'''
+    foodId = max+1
     food_name = request.form["foodName"]
     data1 = int(request.form["nutrient1"])
     data2 = int(request.form["nutrient2"])
@@ -43,12 +42,15 @@ def predict():
     data4 = int(request.form["nutrient4"])
     data5 = int(request.form["nutrient5"])
     data6 = int(request.form["nutrient6"])
+    data7 = request.form["food"]
+    '''Compiling'''
     users_inputdf1 = [foodId,food_name,data1,data2,data3,data4,data5,data6]
     users_inputex = [foodId,data1,data2,data3,data4,data5,data6]
-    example_df1 = df_class.example_df.append(pd.Series(users_inputex, 
-    index = df_class.example_df.columns[:len(users_inputex)]), ignore_index=True)
-    df1 = df_class.df1.append(pd.Series(users_inputdf1, 
-    index=df_class.df1.columns[:len(users_inputdf1)]), ignore_index=True)
+    example_df1 = example_df.append(pd.Series(
+        users_inputex, index = example_df.columns[:len(users_inputex)]), ignore_index=True)
+    dataframe1 = dataframe.append(pd.Series(
+        users_inputdf1, index=dataframe.columns[:len(users_inputdf1)]), ignore_index=True)
+    '''Preprocessing'''
     example_df1 = example_df1.fillna(0)
     ex = StandardScaler().fit_transform(example_df1)
     kmeans = KMeans(n_clusters=4)
@@ -56,16 +58,23 @@ def predict():
     y_predict = kmeans.fit_predict(example_df1)
     example_df1['cluster'] = y_predict
     example_df1 = example_df1[['NDB_No','cluster']]
-    example_df1 = pd.merge(df1, example_df1)
+    example_df1 = pd.merge(dataframe1, example_df1)
+    '''Getting user's output'''
     source = example_df1[example_df1['NDB_No'] == foodId]
     final_food_name = source.iloc[-1]['Shrt_Desc']
     cluster = source.iloc[-1]['cluster']
     final_foodId = source.iloc[-1]['NDB_No']
+    rer = example_df1[example_df1['cluster'] == cluster]
+    reco = rer[rer['Shrt_Desc'].str.contains(data7, case=False)]
     
     return render_template('index.html',
-    prediction_text=f'Your food belongs to cluster number {cluster} with a name of {final_food_name} and food database number of {final_foodId}')
-    #return render_template('index.html', 
-    #prediction_text=f'A food with food name of {final_food_name} that has and food database # of {final_foodId} is clustered in cluster {output}')
+    cluster_text=f'Your food belongs to cluster {cluster} with a name of {final_food_name} and food database number of {final_foodId}',
+    food_name=f'{final_food_name}', reco_text=f'{reco}')
 
+'''@app.route('/reco',methods=['POST'])
+def reco():
+    data7 = request.form["food"]
+    reco = predict.example_df1[predict.example_df1['Shrt_Desc'].str.contains(data7, case=False)]
+    return render_template('idex.html', reco_text=f'{reco}')'''
 if __name__ == "__main__":
     app.run()
