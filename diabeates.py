eb706a97df0fa5b3cae615be1c97d135f84562e1
Nutzbee <tengyteng22@gmail.com>
2023-05-11 22:44:45 +0800

    update

--- diabeates.py
+++ diabeates.py
@@ -233,6 +233,10 @@ class FoodRecommender:
         self.cluster_labels = y_kmeans
 
         return recommended_foods
+    
+
+food_recommender = FoodRecommender()
+food_recommender.preprocess_data()
 
 
 @app.route('/summary', methods=['POST', 'GET'])
@@ -297,8 +301,3 @@ def recommend():
         return jsonify({'recommended_foods': recommended_foods})
     else:
         abort(404, 'No recommendations found')
-
-if __name__ == '__main__':
-    food_recommender = FoodRecommender()
-    food_recommender.preprocess_data()
-    app.run()
